cfc2bac6ab5f3a0a091a2ba032a7546acb0338e1
liweiliang <406320591@qq.com>
2017-12-14 11:05:41 +0800

    loading toast dialog 组件优化

--- src/js/RYui-components.js
+++ src/js/RYui-components.js
@@ -9,7 +9,7 @@
  	if(typeof options == "string" && options == "close"){
  		return RY.dialog.methods[options]();
  	}
- 	var $ryui_dialog = $('<div class="ryui-dialog flex-box flex-align-items-center flex-justify-center"></div>');
+ 	var dialog = $('<div class="ryui-dialog flex-box flex-align-items-center flex-justify-center"></div>');
  	options = $.extend({
  		title:'',
  		width:'',
@@ -17,33 +17,30 @@
  		content:'',
  		target:$("body")
  	},options);
- 	$ryui_dialog.options = options; 
- 	RY.dialog.init($ryui_dialog,param);
- 	return $ryui_dialog;
+ 	dialog.options = options; 
+ 	RY.dialog.init(dialog,param);
+ 	return dialog;
  };
- RY.dialog.init = function($ryui_dialog,param){
- 	var options = $ryui_dialog.options;
- 	$ryui_dialog_mask = $('<div class="ryui-dialog-mask"></div>'),
- 	$ryui_dialog_body = $('<div class="ryui-dialog-body"></div>'),
- 	$ryui_dialog_title = $('<div class="ryui-dialog-title"><h2>'+ options.title +'</h2></div>'),
- 	$ryui_dialog_closeBtn = $('<span class="ryui-dialog-closeBtn"></span>'),
- 	$ryui_dialog_content = $('<div class="ryui-dialog-content"></div>');
- 	$ryui_dialog.append($ryui_dialog_mask).append($ryui_dialog_body);
- 	$ryui_dialog_body.append($ryui_dialog_title).append($ryui_dialog_content);
- 	$ryui_dialog_title.append($ryui_dialog_closeBtn);
+ RY.dialog.init = function(dialog,param){
+ 	var options = dialog.options;
+ 	dialog.mask = $('<div class="ryui-dialog-mask"></div>').appendTo(dialog),
+ 	dialog.body = $('<div class="ryui-dialog-body"></div>').appendTo(dialog),
+ 	dialog.title = $('<div class="ryui-dialog-title"><h2>'+ options.title +'</h2></div>').appendTo(dialog.body),
+ 	dialog.closeBtn = $('<span class="ryui-dialog-closeBtn"></span>').appendTo(dialog.title),
+ 	dialog.content = $('<div class="ryui-dialog-content"></div>').appendTo(dialog.body);
  	if(options.width && options.height){
- 		$ryui_dialog_body.css({'width':options.width,'height':options.height});
+ 		dialog.body.css({'width':options.width,'height':options.height});
  	}else if(options.width){
- 		$ryui_dialog_body.css('width',options.width);
+ 		dialog.body.css('width',options.width);
  	}else if (options.height) {
- 		$ryui_dialog_body.css('height',options.height);
+ 		dialog.body.css('height',options.height);
  	}
- 	$ryui_dialog_closeBtn.on("click",function(){
- 		RY.dialog.methods.close($ryui_dialog);
+ 	dialog.closeBtn.on("click",function(){
+ 		RY.dialog.methods.close(dialog);
  	})
- 	$ryui_dialog_content.append(options.content);
+ 	dialog.content.append(options.content);
  	if(options.target){
- 		options.target.append($ryui_dialog[0]);
+ 		options.target.append(dialog);
  	}
  };
  RY.dialog.methods = {
@@ -60,31 +57,27 @@
  		text:'提示',
  		target:$("body")
  	},options);
- 	var $ryui_toast = $('<div class="ryui-toast"></div>'),
- 	$ryui_toast_body = $('<div class="ryui-toast-body"></div>'),
- 	$ryui_toast_bg = $('<div class="ryui-toast-bg"></div>'),
- 	$ryui_toast_text = $('<div class="ryui-toast-text">'+ options.text +'</div>');
-
- 	$ryui_toast.append($ryui_toast_body);
- 	$ryui_toast_body.append($ryui_toast_bg).append($ryui_toast_text);
-
+ 	var toast = $('<div class="ryui-toast"></div>');
+ 	toast.body = $('<div class="ryui-toast-body"></div>').appendTo(toast);
+ 	toast.bg = $('<div class="ryui-toast-bg"></div>').appendTo(toast.body);
+ 	toast.text = $('<div class="ryui-toast-text">'+ options.text +'</div>').appendTo(toast.body);
  	if(options.width && options.height){
- 		$ryui_toast_body.css({'width':options.width,'height':options.height});
+ 		toast.body.css({'width':options.width,'height':options.height});
  	}else if(options.width){
- 		$ryui_toast_body.css('width',options.width);
+ 		toast.body.css('width',options.width);
  	}else if(options.height){
- 		$ryui_toast_body.css('height',options.height);
+ 		toast.body.css('height',options.height);
  	}
  	if(options.target){
- 		options.target.append($ryui_toast[0]);
+ 		options.target.append(toast);
  	}
  	if(options.holdtime){
  		setTimeout(function(){
- 			$ryui_toast[0].remove();
+ 			toast.remove();
  		},options.holdtime)
  	}
  };
- 
+
  RY.loading = function(options,param){
  	if(typeof options == 'string' && options == 'close'){
  		return RY.loading.methods[options]();
@@ -96,17 +89,18 @@
  		target:$("body")
 
  	},options);
- 	
- 	var ryui_loading_li = '';
+ 	var loading_li = '';
  	for(var i=1;i<=12;i++){
- 		ryui_loading_li += '<li class="ryui-loading-frames-'+ i +'"></li>';
+ 		loading_li += '<li class="ryui-loading-frames-'+ i +'"></li>';
  	}
  	RY.loading.wrap = $('<div class="ryui-loading flex-box flex-align-items-center flex-justify-center"><div class="ryui-loading-mask"></div></div>');
- 	var $ryui_loading_position = $('<div class="ryui-loading-position"></div>');
- 	var $ryui_loading_frames = $('<ul class="ryui-loading-frames ryui-loading-rotate">'+ ryui_loading_li +'</ul>');
- 	$ryui_loading_frames.css({"width":options.width,"height":options.height,"color":options.color});
- 	$ryui_loading_position.append($ryui_loading_frames);
- 	RY.loading.wrap.append($ryui_loading_position);
+ 	var loading_position = $('<div class="ryui-loading-position"></div>').appendTo(RY.loading.wrap);
+ 	var loading_frames = $('<ul class="ryui-loading-frames ryui-loading-rotate">'+ loading_li +'</ul>').appendTo(loading_position);
+ 	loading_frames.css({
+ 		"width":options.width,
+ 		"height":options.height,
+ 		"color":options.color
+ 	});
  	options.target.append(RY.loading.wrap);
 
  };
